4279ee058397fe68707b47f2ddcb6a2286a6ede1
Eden <tit889311k@gmail.com>
2020-12-04 19:46:09 +0500

    Рабочий вариант

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from app import app
+import requests
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    res = requests.get("https://jsonplaceholder.typicode.com/posts").json()
+    return render_template("index.html", posts=res)
